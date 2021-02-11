3834494a377003b542fe16aec3bc6729a779ad36
Shen <71272044+shen02@users.noreply.github.com>
2021-02-11 16:05:51 -0500

    Update scratch.cpp

--- scratch.cpp
+++ scratch.cpp
@@ -1,2 +1,26 @@
-int main() {
+#include <iostream>
+using namespace std;
+
+void mystery1() {
+	int x = 0;
+	int y = 0;
+	for (int ii = 0; ii < 1000000; ++ ii) {
+		x = ((x + 1) * 7) % 97;
+		for (int jj = 0; jj < x; ++ jj) {
+			y = ((y + 1) * 5) % 97;
+		}
+	}
+	cout << x << " " << y << endl;
+}
+
+void mystery2() {
+	int x = 0;
+	int y = 0;
+	for (int ii = 0; ii < 100000; ++ ii) {
+		x = ((x + 1) * 7) % 5011;
+		for (int jj = 0; jj < x; ++ jj) {
+			y = ((y + 1) * 5) % 97;
+		}
+	}
+	cout << x << " " << y << endl;
 }
